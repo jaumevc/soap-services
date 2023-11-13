2486bae5780ee1ed0983c0b9981f3cd48881ac47
jaumevc@me.com <jaumevc@me.com>
2023-11-13 18:02:19 +0100

    commit pendent 2

--- soap-services/src/main/java/com/soap/wsdl/package-info.java
+++ soap-services/src/main/java/com/soap/wsdl/package-info.java
@@ -2,7 +2,7 @@
 // Este archivo ha sido generado por Eclipse Implementation of JAXB v2.3.7 
 // Visite https://eclipse-ee4j.github.io/jaxb-ri 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2023.11.11 a las 08:39:12 PM CET 
+// Generado el: 2023.11.13 a las 06:01:42 PM CET 
 //
 
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://tempuri.org/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
