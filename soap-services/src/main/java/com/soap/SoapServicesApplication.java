0016a03878e476e51adf350b5d40b5785cd5f8cf
jaumevc <jaumevc@me.com>
2023-11-14 08:36:55 +0100

    resposta operacions creades

--- soap-services/src/main/java/com/soap/SoapServicesApplication.java
+++ soap-services/src/main/java/com/soap/SoapServicesApplication.java
@@ -13,20 +13,19 @@ import com.soap.wsdl.AddResponse;
 @SpringBootApplication
 public class SoapServicesApplication {
 
-	private static final Logger LOG = LoggerFactory.getLogger(SoapServicesApplication.class); 
+//	private static final Logger LOG = LoggerFactory.getLogger(SoapServicesApplication.class); 
 			
 	public static void main(String[] args) {
 		SpringApplication.run(SoapServicesApplication.class, args);
 	}
-	
-	@Bean
-	CommandLineRunner init(SoapClient soapClient) {
-		
-		return args ->{
-			AddResponse resultatSuma = soapClient.getAddResponse(10, 5);
-			
-			LOG.info("El resultat de la suma de {} i {} és: {}", 10, 5, resultatSuma.getAddResult());
-		};
-	}
 
+	//	@Bean
+//	CommandLineRunner init(SoapClient soapClient) {
+//		
+//		return args ->{
+//			AddResponse resultatSuma = soapClient.getAddResponse(10, 5);
+//			
+//			LOG.info("El resultat de la suma de {} i {} és: {}", 10, 5, resultatSuma.getAddResult());
+//		};
+//	}
 }
--- soap-services/src/main/java/com/soap/client/SoapClient.java
+++ soap-services/src/main/java/com/soap/client/SoapClient.java
@@ -4,12 +4,22 @@ import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
 import org.springframework.ws.soap.client.core.SoapActionCallback;
 
 import com.soap.wsdl.AddResponse;
+import com.soap.wsdl.SubtractResponse;
+import com.soap.wsdl.DivideResponse;
+import com.soap.wsdl.MultiplyResponse;
 import com.soap.wsdl.Add;
+import com.soap.wsdl.Subtract;
+
+import com.soap.wsdl.Multiply;
+
+import com.soap.wsdl.Divide;
 
 //enlloc de marcar la anotacio @Component creo un package de configuracio
 public class SoapClient extends WebServiceGatewaySupport{
 	
 	//Aqui definirem els nostres mètodes:suma, resta...
+	
+	//Suma
 	//Si mirem les classes que ens crea el pom, veiem que tenim una resposta de la suma: AddResponse
 	public AddResponse getAddResponse(int numA, int numB) {
 		
@@ -24,7 +34,43 @@ public class SoapClient extends WebServiceGatewaySupport{
 		return addResponse;
 	}
 	
+	//Resta:
+	public SubtractResponse getSubstractResponse(int numA, int numB) {
+		Subtract restaRequest = new Subtract();
+		restaRequest.setIntA(numA);
+		restaRequest.setIntB(numB);
+		
+		//li passem el targetNameSpace del wsdl
+		SoapActionCallback soapActionCallback = new SoapActionCallback("http://tempuri.org/Subtract");
+		
+		SubtractResponse restaResponse = (SubtractResponse) getWebServiceTemplate().marshalSendAndReceive("http://www.dneonline.com/calculator.asmx", restaRequest,soapActionCallback);
+		
+		return restaResponse;
+	}
 	
+	//Producte
+	public MultiplyResponse getMultiplyResponse(int numA, int numB) {
+		Multiply producteRequest = new Multiply();
+		producteRequest.setIntA(numA);
+		producteRequest.setIntB(numB);
+		
+		//li passem el targetNameSpace del wsdl
+		SoapActionCallback soapActionCallback = new SoapActionCallback("http://tempuri.org/Multiply");
+		
+		MultiplyResponse productResponse = (MultiplyResponse) getWebServiceTemplate().marshalSendAndReceive("http://www.dneonline.com/calculator.asmx", producteRequest, soapActionCallback);
+		
+		return productResponse;
+	}
 	
-
+	public DivideResponse getDivideResponse(int numA, int numB) {
+		Divide divideRequest = new Divide();
+		divideRequest.setIntA(numA);
+		divideRequest.setIntB(numB);
+		
+		//li passem el targetNameSpace del wsdl
+		SoapActionCallback soapActionCallback = new SoapActionCallback("http://tempuri.org/Divide");
+		
+		DivideResponse divideResponse = (DivideResponse) getWebServiceTemplate().marshalSendAndReceive("http://www.dneonline.com/calculator.asmx", divideRequest, soapActionCallback);
+		return divideResponse;
+	}
 }
